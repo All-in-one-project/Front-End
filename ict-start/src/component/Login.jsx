5484c17618b959b4307b294e49093b57c27d96cb
rinna <chaerin0623@naver.com>
2024-06-17 17:07:14 +0900

    9-commit

--- ict-start/src/component/Login.jsx
+++ ict-start/src/component/Login.jsx
@@ -8,17 +8,9 @@ function Login() {
   const [error, setError] = useState('');
   const history = useHistory(); // useHistory 훅을 사용하여 페이지 이동을 제어
 
-  const handleIdChange = (event) => {
-    setId(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
 
   const handleLogin = async (event) => {
     event.preventDefault(); 
-
     if (id === 'test' && password === 'password') {
       // 로그인 성공 시 홈 페이지로 이동
       history.push('/home');
@@ -42,24 +34,27 @@ function Login() {
             id="id" 
             type="login" 
             value={id} 
-            onChange={handleIdChange} 
+            onChange={(event)=>setId(event.target.value)}
             placeholder="아이디/학번 입력" 
             required 
             className="login-input" //  스타일 적용을 위해 className 추가
           />
         </div>
+      
         <div>
           <input 
             id="password" 
             type="password" 
             value={password} 
-            onChange={handlePasswordChange} 
+            onChange={(event)=>setPassword(event.target.value)}
             placeholder="비밀번호 입력" 
-            required 
+            required //입력 필드를 채워야만하는 필수 입력
             className="login-input" 
           />
         </div>
+
         {error && <div className="error-message">{error}</div>} {/* 에러 메시지 표시 */}
+
         <button type="submit" className="login-button">로그인</button> 
       </form>
        <div className="separator"></div> 
